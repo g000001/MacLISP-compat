7651bc6b18ca40e3521b46316537a4dc53af4b52
g000001 <chiba.masaomi@gmail.com>
2010-12-23 17:43:28 +0900

    課題: 分配束縛なletとerrorの形式

--- CARCDR.lisp
+++ CARCDR.lisp
@@ -1,4 +1,3 @@
-
 ;;; -*-LISP-*-
 
 ;;; Purpose:  to permit long names, like CADADADADDDR, to be easily
@@ -11,30 +10,33 @@
 ;;;    LAMBDA form with a composition of initial carcdr functions
 ;;;    of up to 4 deep, which should be (already) defined primitively.
 
+(IN-PACKAGE :MACLISP)
 
-
-(DEFMACRO DEF-CARCDR L 
-    `(PROGN 'COMPILE 
-	    ,@(mapcar '(lambda (x) `(DEFPROP ,x C*R MACRO)) l)))
+(DEFMACRO DEF-CARCDR (&rest carcdrs)
+    `(PROGN ,@(mapcar (lambda (x)
+                        `(defun ,x (list)
+                           ,(c*r (list x 'list))))
+                      carcdrs)))
 
 (DEFUN C*R (X)
-   (DECLARE (SPECIAL CARCDR))		;Gets the complr's CARCDR variable
-   (LET (((NAME ARG1 . L) X))
-	(AND L (ERROR '|Extra args in call to C*R macro| X 'WRNG-NO-ARGS))
+#|   (LET (((NAME ARG1 . L) X))|#
+     (destructuring-bind (NAME ARG1 . L) X
+	(AND L (ERROR 'WRNG-NO-ARGS
+                      :message "~A Extra args in call to C*R macro"
+                      :argument X))
 	(AND (OR (< (LENGTH (SETQ L (EXPLODEC NAME))) 7)
 		 (NOT (EQ (CAR L) 'C))
 		 (NOT (EQ (CAR (SETQ L (NREVERSE (CDR L)))) 'R))
-		 (DO L (SETQ L (NREVERSE (CDR L))) (CDR L) (NULL L)
-		     (AND (NOT (MEMQ (CAR L) '(A D))) (RETURN 'T))))
-	     (ERROR '|Invalid name for C*R macro| X 'WRNG-TYPE-ARG))
-	`(,(COND ((EQ COMPILER-STATE 'COMPILE) `(,carcdr ,@(nreverse l)))
-		 (`(LAMBDA (X) ,(|c*r-expander/|| l 'X))))
-	  ,arg1)))
+		 (DO ((L (SETQ L (NREVERSE (CDR L))) (CDR L))) ((NULL L))
+		     (AND (NOT (MEMBER (CAR L) '(A D))) (RETURN 'T))))
+	     (ERROR 'WRNG-TYPE-ARG
+                    :message "~A Invalid name for C*R macro|"
+                    :argument X))
+	`((LAMBDA (X) ,(|c*r-expander\|| l 'x))
+          ,arg1)))
 
-(DEFUN |c*r-expander/|| (L ARG) 
+(DEFUN |c*r-expander\|| (L ARG)
   (COND ((< (LENGTH L) 5) `(,(implode (nconc (list 'C) l '(R))) ,arg))
-	((LET* ((/3TAIL (NTHCDR 3 L)) (/4TAIL (CDR /3TAIL)))
-	       (RPLACD /3TAIL () )
-	       (|c*r-expander/|| L (|c*r-expander/|| /4TAIL ARG))))))
-
-
+	((LET* ((3TAIL (NTHCDR 3 L)) (4TAIL (CDR 3TAIL)))
+	       (RPLACD 3TAIL () )
+	       (|c*r-expander\|| L (|c*r-expander\|| 4TAIL ARG))))))
