a2759b938f2c6b7eefbd69273b66c3aa00f0f369
g000001 <chiba.masaomi@gmail.com>
2010-12-23 17:45:17 +0900

    CARCDRを移植

--- base.lisp
+++ base.lisp
@@ -1,7 +1,11 @@
 ;; -*-LISP-*-
 (IN-PACKAGE :MACLISP)
 
-(define-condition wrng-type-arg (simple-error)
+(defmacro keep (&body body)
+  (declare (ignore body))
+  (values))
+
+(define-condition wrng-type-arg (error)
   ((argument :reader wrng-type-arg-argument :initarg :argument)
    (message :reader wrng-type-arg-message :initarg :message))
   (:report (lambda (condition stream)
@@ -9,9 +13,24 @@
                      (wrng-type-arg-message condition)
                      (wrng-type-arg-argument condition)))))
 
+(define-condition wrng-no-args (error)
+  ((argument :reader wrng-no-args-argument :initarg :argument)
+   (message :reader wrng-no-args-message :initarg :message))
+  (:report (lambda (condition stream)
+             (format stream
+                     (wrng-no-args-message condition)
+                     (wrng-no-args-argument condition)))))
+
 (defun exploden (expr)
   (map 'list
     (lambda (x) (char-code x))
+    (princ-to-string
+     (read-from-string
+      (write-to-string expr)))))
+
+(defun explodec (expr)
+  (map 'list
+    (lambda (x) (intern (string x)))
     (princ-to-string
      (read-from-string
       (write-to-string expr)))))
@@ -40,3 +59,37 @@
 (defun alphalessp (x y)
   (null (not (string< x y))))
 
+(setf (symbol-function 'add1) #'1+)
+(setf (symbol-function 'greaterp) #'>)
+(setf (symbol-function 'lessp) #'<)
+(setf (symbol-function 'DIFFERENCE) #'-)
+(setf (symbol-function 'DIFFERENCE) #'-)
+(setf (symbol-function 'REMAINDER) #'rem)
+
+(defun bigp (obj)
+  (typep obj 'bignum))
+
+(defun lsh (integer count)
+  (ash (- integer) count))
+
+(defun minus (&optional number)
+  (if number
+      (- number)
+      0))
+
+(cardinal 8.8)
+
+(fnorm 8)
+
+
+;; implode
+    (EQ 'ABC (IMPLODE '(A B C)))   =>   T
+
+    (IMPLODE '(A #\SPACE #\B))      =>   |A B|
+
+    (IMPLODE (EXPLODE 3))           =>   /3
+
+    (NUMBERP (IMPLODE (EXPLODE 3))) =>   NIL
+
+(defun implode (expr)
+  (values (intern (format nil "~{~A~}" expr))))
--- package.lisp
+++ package.lisp
@@ -1,18 +1,28 @@
 (defpackage :maclisp
   (:use :cl)
-  (:export :iota
-           :phi
-           :phi
-           :pi
-           ;; APROPOS
-           :apropos
-           :apropos-sorted
-           ;; BS
-           :timit
-           :ntimit
-           )
-  (:shadow :pi
-           :apropos))
+  (:export
+   ;; IOTA
+   :iota
+   :phi
+   :pi
+   ;; APROPOS
+   :apropos
+   :apropos-sorted
+   ;; BS
+   :timit
+   :ntimit
+   )
+  (:shadow
+   ;; IOTA
+   :pi
+   ;; APROPOS
+   :apropos
+   ;; SETS
+   :union
+   :subsetp
+   ;; CARCDR
+   :def-carcdr
+   ))
 
 (in-package :maclisp)
 
